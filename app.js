4fcafde9db7159b3ce6e50a271ab9560c3a1d2d5
NicolasArayaB <nicolasaraya@gmail.com>
2021-01-23 14:56:54 +0000

    w/ forEach

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-  let pronoun = ['the','our', 'last'];
-  let adj = ['great', 'big', 'of'];
-  let noun = ['jogger','racoon', 'us'];
-  let extensions = ['.com', '.net', '.cl', '.io', '.us']
+let pronoun = ['the','our', 'last'];
+let adj = ['great', 'big', 'of'];
+let noun = ['jogger','racoon', 'us'];
+let extensions = ['.com', '.net', '.cl', '.io', '.us'];
+//
+//for (let p of pronoun)
+//  for (let a  of adj)
+//      for (let n of noun)
+//          for (let e of extensions)
+//              if (e.includes(n)){
+//                  console.log(p + a + e);
+//              } else {
+//                  console.log(p + a + n + e);
+//              }
 
-  for (let p of pronoun)
-    for (let a  of adj)
-        for (let n of noun)
-            for (let e of extensions)
+pronoun.forEach(p => {
+    adj.forEach(a => {
+        noun.forEach(n => {
+            extensions.forEach(e => {
                 if (e.includes(n)){
                     console.log(p + a + e);
                 } else {
-                    console.log(p + a + n + e);
-                }
-                
+                console.log(p + a + n + e);
+            }})
+        })
+    })
+});
